4c9ae4810d7b8978a1e9d04d90126ea01768cf43
Nathan Coburn <nathanlcoburn@gmail.com>
2018-12-20 01:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+const newDriver = Object.assign({}, driver, {[key]: value});
 
   driver[key] = value;
   
