2eee072f687078feba38dfcf20656f7dbed5fdf9
Nathan Coburn <nathanlcoburn@gmail.com>
2018-12-20 01:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const newDriver = Object.assign({}, driver);
 function destructivelyDeleteFromDriverByKey(driver, key, value) {
 const newDriver = Object.assign({}, driver);
  
+  newDriver.driver = newDriver;
   delete newDriver[key]
 
   return newDriver;
